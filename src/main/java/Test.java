9460f24bcaefbfbab4d4ce5dfd3ec648263e6c49
riven-yrw <rivenyrw@163.com>
2022-07-28 16:53:00 +0800

    hot-fix commit1

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -2,5 +2,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("11111");
         System.out.println("11322");
+        System.out.println("hot-fox");
     }
 }
